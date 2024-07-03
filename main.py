e5e4633d18edbe870a9b0ac2b029e735d22f46d7
asmakhans <78252076+asmakhans@users.noreply.github.com>
2024-07-03 13:00:55 -0600

    Update main.py
    need to work on how we're returning the converted index-to-text and vice versa, we need to return an array not a single item.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import string
+
 import numpy as np
 
 # assign each element(letter) into its corresponding indexed value(starting from 0)
 alphabet_mod26 = 'abcdefghijklmnopqrstuvwxyz'
 alphabet_mod29 = 'abcdefghijklmnopqrstuvwxyz ?!'  # this includes _ ? !
 
-index = range(len(alphabet_mod26))
+index = range(len(alphabet_mod26)) #char in alphabet_mod26
+# index_29 = range(len(alphabet_mod29))
 
 # letter_to_index = dict(zip(index, alphabet_mod26))
+# we need for letter to index to return an array, so let's make this into a function that goes throguh each char in the text
+# this is where we need to return an array, since at the moment it is only returning a value of 1
+
 # index_to_letter = dict(zip(alphabet_mod26, index))
+# this part will be a bit more complicated since we have to return a .join version of the array according to the mappings of the index
 
 # matrices for encryption
 key_26 = np.array([[6, 11], [25, 15]])
@@ -23,14 +30,15 @@ def encrypt(message, key, mod):
     message.lower()
 
     # mapping
-    letter_to_index = dict(zip(index, alphabet_mod26))
+    # letter_to_index = dict(zip(index, alphabet_mod26)) # A: 0
+    letter_to_index = dict(enumerate(alphabet_mod26, 0))
+
 
     # MOVE THIS FOR LOOP INTO ITS OWN FUNCTION
     # example: 13671
     for i in range(0, len(letter_to_index), 2):
-        block = np.array(letter_to_index[i:i+2])
-        # block = np.array([letter_to_index])
-        # block.reshape(2, len(letter_to_index)/2)
+        # block = np.array(letter_to_index[i:i+2])
+        block = np.array([letter_to_index]).reshape(-1, 2).T
         # print(block)
         # atp: 13 67 1x
         # dot product of the arrays (using the key and the newly created blocks) then modded to either 26 or 29
@@ -42,3 +50,15 @@ def encrypt(message, key, mod):
 
 text = "TRYTOBREAKTHISCODE"
 print(encrypt(text, key_26, 26))
+
+
+
+
+
+
+
+
+
+
+
+
