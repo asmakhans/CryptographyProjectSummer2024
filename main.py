fdad07a25437896eb2a43d74abfca65ff0e63dee
London-Fitches <londonfitches@gmail.com>
2024-07-23 11:56:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -35,42 +35,42 @@ def numbers_to_text(numbers, index_to_letter):
 def text_to_num(text, letter_to_index):
     return [letter_to_index[char] for char in text]
 
-# Create pairs from numerical values
-def create_pairs(numbers):
-    pairs = []
+# Create bigrams from numerical values
+def create_bigrams(numbers):
+    bigrams = []
     # Iterate through the list of numbers in steps of 2
     for i in range(0, len(numbers), 2):
         pair = (numbers[i], numbers[i + 1])  # Create a pair using current and next element
-        pairs.append(pair)  # Add each pair to the pairs list
-    return pairs
+        bigrams.append(pair)  # Add each bigram to the bigrams list
+    return bigrams
 
 
-# Frequency analysis of the pairs from an intercepted encrypted message
+# Frequency analysis of the bigrams from an intercepted encrypted message
 def frequency_analysis(encrypted_message):
-    encrypted_pairs = create_pairs(encrypted_message)
-    total_pairs = len(encrypted_pairs) - 1
+    encrypted_bigrams = create_bigrams(encrypted_message)
+    total_bigrams = len(encrypted_bigrams) - 1
     frequency = {}
 
     # Counts how many times each pair occurs within the encrypted message
-    for pair in encrypted_pairs:
-        if pair in frequency:
-            frequency[pair] += 1
+    for bigram in encrypted_bigrams:
+        if bigram in frequency:
+            frequency[bigram] += 1
         else:
-            frequency[pair] = 1
+            frequency[bigram] = 1
 
     # Calculate the frequency of each pair as a percentage
     frequency_percentage = {}
-    for pair, number_of_pair in frequency.items():
-        frequency_percentage[pair] = (number_of_pair / total_pairs) * 100
+    for bigram, number_of_bigrams in frequency.items():
+        frequency_percentage[bigram] = (number_of_bigrams / total_bigrams) * 100
 
     # Sort the frequency_percent dictionary by frequency in descending order
     sorted_frequency_percent = dict(sorted(frequency_percentage.items(), key=lambda item: item[1], reverse=True))
 
     return sorted_frequency_percent
 
-    # create "Frequency of Character Pairs in English Language Text" dictionary
-    # compare frequency_percentage to "Frequency of Character Pairs in English Language Text" dictionary
-    # assign like pairs together?
+    # create "Frequency of Character bigrams in English Language Text" dictionary
+    # compare frequency_percentage to "Frequency of Character bigrams in English Language Text" dictionary
+    # assign like bigrams together?
 
 
 # Encryption function using Hill 2-cipher
@@ -85,30 +85,30 @@ def encrypt(message, key, mod, letter_to_index, index_to_letter):
     # Convert the message to numerical values
     message_nums = text_to_num(message, letter_to_index)
 
-    # Create pairs from the numerical values
-    pairs = create_pairs(message_nums)
+    # Create bigrams from the numerical values
+    bigrams = create_bigrams(message_nums)
 
-    encrypted_pairs = []
-    for pair in pairs:
+    encrypted_bigrams = []
+    for bigram in bigrams:
         # construct the pair as an array
-        array = np.array(pair)
+        array = np.array(bigram)
 
         # Multiply the key matrix with the vector
         encrypted_array = np.dot(key, array) % mod
 
-        # convert to list and append to encrypted_pairs
-        encrypted_pair = encrypted_array.tolist()
-        encrypted_pairs.append(encrypted_pair)
+        # convert to list and append to encrypted_bigrams
+        encrypted_bigram = encrypted_array.tolist()
+        encrypted_bigrams.append(encrypted_bigram)
 
     encrypted_nums = []
-    for p in encrypted_pairs:
+    for p in encrypted_bigrams:
         for i in p:
             encrypted_nums.append(i)
     encrypted_text = numbers_to_text(encrypted_nums, index_to_letter)
 
     return encrypted_text
 
-
+# Decrypt function using Hill 2-cipher
 def decrypt():
     return
 
